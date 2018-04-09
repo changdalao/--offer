a4b904078f2a8737e51bc1f06e7e53c050434c35
pangdun <32231779+changdalao@users.noreply.github.com>
2018-04-09 22:51:07 +0800

    替换字符串中的空格

--- ReplaceBlack.c
+++ ReplaceBlack.c
@@ -1,5 +1,6 @@
+//方法1：
 //时间复杂度O(n2)
-void ReplaceBlack(char* arr,char* key,int len)
+void ReplaceBlack1(char* arr,char* key,int len)
 {
 	if (NULL == arr || NULL == key)
 		return;
@@ -25,12 +26,39 @@ void ReplaceBlack(char* arr,char* key,int len)
 		cur++;
 	}
 }
+ //方法2：
+void ReplaceBlack2(char* arr, char* key, int len)
+{
+	char* cur = arr;
+	int count = 0;
+	while (*cur)//统计空格个数
+	{
+		if (*cur == ' ')
+			count++;
+		cur++;
+	}
+	int newlength = len + count * (strlen(key) - 1);//新空间
+	int indexlen = len;
+	int indexnewlen = newlength;
+	while (indexlen >= 0 && indexnewlen > indexlen)
+	{
+		if (arr[indexlen] == ' ')
+		{
+			arr[indexnewlen--] = '0';
+			arr[indexnewlen--] = '2';
+			arr[indexnewlen--] = '%';
+		}
+		else
+			arr[indexnewlen--] = arr[indexlen];
+		--indexlen;
+	}
+}
+
 void Test()
 {
 	char arr[100] = " welcome to the university! ";
 	printf("原字符串>%s\n",arr);
 	char key[] = "%20";
-	ReplaceBlack(arr, key, strlen(arr)-1);
+	ReplaceBlack1(arr, key, strlen(arr)-1);
 	printf("变换后>%s\n", arr);
 }
- 
