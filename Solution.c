043d87c04d5fa4cb689b1b6ea5fdfa150fc7ba0e
pangdun <32231779+changdalao@users.noreply.github.com>
2018-05-28 21:41:45 +0800

    正则表达式替换

---
+++ Solution.c
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool match(char* str, char* pattern)
+    {
+		 if (*str == NULL && *pattern == NULL)
+                return true;
+            if (*str != NULL && *pattern == NULL)
+                return false;
+            if (*(pattern+1) != '*')
+            {
+                if (*str == *pattern || (*str != '\0' && *pattern == '.'))
+                    return match(str+1, pattern+1);
+                else
+					return false;
+            }
+            else 
+            {
+                if (*str == *pattern || (*str != '\0' && *pattern == '.'))
+                    return match(str, pattern+2) || match(str+1, pattern);
+                else
+               return match(str, pattern+2);
+            }
+    }
+};
+int main()
+{
+	Solution s;
+	char* str="aaa";
+	char* pat="aa*a";
+	char* pt="ab*ac*a";
+	cout<<s.match(str,pat)<<endl;
+	system("pause");
+	return 0;
+}
