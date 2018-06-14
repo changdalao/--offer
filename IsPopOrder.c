c09086a38310be82347b4b5f1fc346573ccc6838
pangdun <32231779+changdalao@users.noreply.github.com>
2018-06-14 23:09:44 +0800

    判断栈的出栈序列是否合法

--- IsPopOrder.c
+++ IsPopOrder.c
@@ -1,23 +1,39 @@
 class Solution {
 public:
 	bool IsPopOrder(vector<int> pushV,vector<int> popV) {
-		stack<int> s;
-		vector<int>::iterator it=popV.begin();
-		vector<int>::iterator itp=pushV.begin();
-		while(!pushV.empty() || s.top()!=(*it)){
-			if(itp < pushV.end()){
-				s.push(*itp);
-				itp++;
+		if(pushV.empty())
+			return false;
+		stack<int> temp;
+		for(int i=0,j=0;i<pushV.size();i++)
+		{
+			temp.push(pushV[i]);
+			while(j<popV.size()&&temp.top()==popV[j])//根据出队表来对我临时的栈做出队操作
+			{
+				temp.pop();
+				j++;
 			}
-			else
-				;
-			if(s.top()==*it&&!s.empty()){
-				s.pop();
-				it++;
-			}
-			if(pushV.empty()&&!popV.empty() || s.top()!=*it)
-				return false;
 		}
-			return true;
+		return temp.empty();
 	}
 };
+int main()
+{
+	Solution so;
+	vector<int> v1;
+	v1.push_back(1);
+	v1.push_back(2);
+	v1.push_back(3);
+	v1.push_back(4);
+	v1.push_back(5);
+
+	vector<int> v2;
+	v2.push_back(4);
+	v2.push_back(5);
+	v2.push_back(3);
+	v2.push_back(2);
+	v2.push_back(1);
+	
+	cout<<so.IsPopOrder(v1,v2)<<endl;
+	system("pause");
+	return 0;
+}
