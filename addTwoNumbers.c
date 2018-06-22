8c384cdc4117e8003e0e380b94696f60743c946b
pangdun <32231779+changdalao@users.noreply.github.com>
2018-06-22 22:58:45 +0800

    两个链表相加，注意进位

--- addTwoNumbers.c
+++ addTwoNumbers.c
@@ -30,3 +30,20 @@ public:
 		return l2;
 	}
 };
+int main()
+{
+	Solution s;
+	ListNode* node1=new ListNode(2);
+	ListNode* node2=new ListNode(4);
+	ListNode* node3=new ListNode(3);
+	node1->next=node2;
+	node2->next=node3;
+	ListNode* node4=new ListNode(5);
+	ListNode* node5=new ListNode(6);
+	ListNode* node6=new ListNode(6);
+	node4->next=node5;
+	node5->next=node6;
+	ListNode* result=s.addTwoNumbers(node1,node4);
+	system("pause");
+	return 0;
+}
